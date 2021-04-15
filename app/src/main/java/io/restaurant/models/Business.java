88aa00d116949c1148800840c2957ec7ab9b5d11
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-04-15 15:57:08 +0200

    Add parcelables and PagerAdapter

--- app/src/main/java/io/restaurant/models/Business.java
+++ app/src/main/java/io/restaurant/models/Business.java
@@ -1,9 +1,14 @@
 
 package io.restaurant.models;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+import java.util.List;
+
+@Parcel
 
 public class Business {
 
